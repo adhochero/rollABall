c6dd338b4e3a5c08ca38183471405d46b6cf4e9c
mathew <mathewdeanv@gmail.com>
2022-10-26 14:34:47 -0600

    added text map environment and wall collisions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import {Entity} from './entity.js'
 import {Input} from './input.js'
+import {Entity} from './entity.js'
+import {Environment} from './environment.js'
 
 let canvas;
 let context;
@@ -9,6 +10,9 @@ let fps;
 let input;
 let keys = [];
 let entities;
+let environment;
+let follow = {x: 0, y: 0};
+let followSpeed = 0.05;
 
 
 window.onload = init;
@@ -18,6 +22,8 @@ function init(){
     context = canvas.getContext('2d');
     canvas.width = 666;
     canvas.height = 500;
+
+    environment = new Environment();
 
     input = new Input(keys);
     entities = [
@@ -40,6 +46,9 @@ function init(){
             entity.velocity.y = Math.random() * 400 - 200;
         }
     });
+
+    follow.x = -entities[0].position.x + canvas.width / 2;
+    follow.y = -entities[0].position.y + canvas.height / 2;
 
     //start the first frame request
     window.requestAnimationFrame(gameLoop);
@@ -68,7 +77,7 @@ function update(secondsPassed){
     entities.forEach(entity => {
         entity.update(secondsPassed);
 
-        //check for collisions
+        //check for entity collisions
         entities.forEach(otherEntity => {
             if(otherEntity === entity) return;
             
@@ -103,6 +112,36 @@ function update(secondsPassed){
                 otherEntity.velocity.x += (speed * collisionNormal.x);
                 otherEntity.velocity.y += (speed * collisionNormal.y);
             }
+        });
+
+        //check for wall collisions
+        environment.walls.forEach(wall => {
+            if(checkCircleToRectCollision(entity, wall)){
+                if (entity.position.x + entity.moveDirection.x > 
+                    (wall.x - wall.width/2) - entity.width/2 &&
+                    entity.position.x < (wall.x - wall.width/2)){
+                    entity.velocity.x = -entity.moveDirection.x;
+                    entity.position.x = wall.x - wall.width/2 - entity.width/2;
+                }
+                else if (entity.position.x + entity.moveDirection.x < 
+                    (wall.x + wall.width/2) + entity.width/2 &&
+                    entity.position.x > (wall.x + wall.width/2)){
+                    entity.velocity.x = -entity.moveDirection.x;
+                    entity.position.x = wall.x + wall.width/2 + entity.width/2;
+                }
+                else if (entity.position.y + entity.moveDirection.y > 
+                    (wall.y - wall.height/2) - entity.height/2 &&
+                    entity.position.y < (wall.y - wall.height/2)){
+                    entity.velocity.y = -entity.moveDirection.y;
+                    entity.position.y = wall.y - wall.height/2 - entity.height/2;
+                }
+                else if (entity.position.y + entity.moveDirection.y < 
+                    (wall.y + wall.height/2) + entity.height/2 &&
+                    entity.position.y > (wall.y + wall.height/2)){
+                    entity.velocity.y = -entity.moveDirection.y;
+                    entity.position.y = wall.y + wall.height/2 + entity.height/2;
+                }
+            }
         })
     });
     
@@ -112,16 +151,48 @@ function draw(){
     //clear the entire canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    follow.x = lerp(follow.x, -entities[0].position.x + canvas.width / 2, followSpeed);
+    follow.y = lerp(follow.y, -entities[0].position.y + canvas.height / 2, followSpeed);
+
+    context.save();
+    context.translate(follow.x, follow.y);
+
+    environment.draw(context);
+
     entities.forEach(entity => {
         entity.draw(context);
     });
+
+    context.restore();
+}
+
+function lerp(start, end, t){
+    return  (1 - t) * start + end * t;
 }
 
 function checkCircleCollision(cir1, cir2){
-    //assumes x and y pos is center of circles
+    //assumes x and y pos is center
     let dx = cir1.position.x - cir2.position.x;
     let dy = cir1.position.y - cir2.position.y;
     let radiusSum = cir1.width * 0.5 + cir2.width * 0.5;
 
     return (dx * dx + dy * dy <= radiusSum * radiusSum)
+}
+
+function checkCircleToRectCollision(cir, rect){
+    //assumes x and y pos is center
+    let distance = {
+        x: Math.abs(cir.position.x - rect.x),
+        y: Math.abs(cir.position.y - rect.y)
+    };
+
+    if(distance.x > rect.width/2 + cir.width/2) return false;
+    if(distance.y > rect.height/2 + cir.height/2) return false;
+
+    if(distance.x <= rect.width/2) return true;
+    if(distance.y <= rect.height/2) return true;
+
+    let dx = distance.x - rect.width/2;
+    let dy = distance.y - rect.height/2;
+    return (dx * dx + dy * dy <= cir.width/2 * cir.width/2);
 }
